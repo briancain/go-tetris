f517a6d73184ce0486c5742d4c7631979bf9d668
Brian Cain <briancq@amazon.com>
2025-10-28 08:57:14 -0700

    infra: Add A record for ALB

--- terraform/ssl.tf
+++ terraform/ssl.tf
@@ -40,6 +40,21 @@ data "aws_route53_zone" "main" {
   private_zone = false
 }
 
+# A record pointing domain to ALB
+resource "aws_route53_record" "api" {
+  count = var.enable_ssl && var.domain_name != "" ? 1 : 0
+
+  zone_id = data.aws_route53_zone.main[0].zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
+
 # Certificate validation
 resource "aws_acm_certificate_validation" "main" {
   count = var.enable_ssl && var.domain_name != "" ? 1 : 0
